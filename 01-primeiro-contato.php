6e07efb24f505651b367efbf0718b04cff22359b
Rafaela Cristina Alcântara da Silva <124393270+rafaela13c@users.noreply.github.com>
2026-07-04 15:31:28 -0300

    Create 01-primeiro-contato.php

---
+++ 01-primeiro-contato.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Autor  : Rafaela Cristina Alcântara da Silva
+ * GitHub : https://github.com/rafaela13c
+ * LinkedIn: https://www.linkedin.com/in/rafaela-cas1308/
+ *
+ * Descrição: Arquivo 01/30 - O Primeiro Contato (Código Amador Inicial)
+ */
+
+// Primeiro programa: apenas exibir uma mensagem na tela
+echo "Olá, mundo! Este é o meu primeiro código em PHP.";
